3b20d0819d9f0d58f6d70268dc63faf41a3cd0a9
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-27 02:53:24 +0100

    fix(add naming to pathvariabl and remove try/catch's)

--- aid_compass_application/src/main/java/aidcompass/api/user/UserController.java
+++ aid_compass_application/src/main/java/aidcompass/api/user/UserController.java
@@ -40,144 +40,67 @@ public class UserController {
     public ResponseEntity<?> createUser(@RequestBody @Valid UserRegistrationDto userRegistrationDto,
                                         BindingResult bindingResult, Locale locale){
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Info", "Creating user");
-
         if(bindingResult.hasErrors()){
             ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
                     this.messageSource.getMessage("400", null, "error.400", locale));
             problemDetail.setProperty("errors", MapUtils.bindingErrors(bindingResult));
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
-                    .headers(httpHeaders)
                     .body(problemDetail);
         }
-        try{
-            userService.save(userRegistrationDto);
-//            httpHeaders.setLocation(URI.create("/customer/login"));
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .headers(httpHeaders)
-                    .build();
-        }catch (Exception e){
-            log.error("Error occurred while creating the user: ", e);
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
-        }
+
+        userService.save(userRegistrationDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<?> updateUser(@RequestBody UserRegistrationDto userRegistrationDto,
-                                        @PathVariable @Positive Long id, Locale locale){
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Info", "Updating user");
+                                        @PathVariable("id") @Positive Long id, Locale locale){
+
         if (!userService.existingById(id))
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .headers(httpHeaders)
-                    .build();
+            throw new EntityNotFoundException();
         UserUpdateDto userUpdateDto = userService.mapToUpdateDto(userRegistrationDto);
         userUpdateDto.setId(id);
-        try {
-            Set<ConstraintViolation<UserUpdateDto>> bindingResult = validator.validate(userUpdateDto);
-            if(!bindingResult.isEmpty()){
-                ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                        this.messageSource.getMessage("400", null, "error.400", locale));
-                problemDetail.setProperty("error", MapUtils.bindingErrorsFromConstraintValidatorContext(bindingResult));
-                return ResponseEntity
-                        .status(HttpStatus.BAD_REQUEST)
-                        .headers(httpHeaders)
-                        .body(problemDetail);
-            }
-        } catch (Exception e){
-            log.error("Error occurred while validate user: ", e);
+
+        Set<ConstraintViolation<UserUpdateDto>> bindingResult = validator.validate(userUpdateDto);
+        if(!bindingResult.isEmpty()){
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                    this.messageSource.getMessage("400", null, "error.400", locale));
+            problemDetail.setProperty("error", MapUtils.bindingErrorsFromConstraintValidatorContext(bindingResult));
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(problemDetail);
         }
-        try{
-            userService.update(userUpdateDto);
-        } catch (Exception e){
-            log.error("Error occurred while updating the user: ", e);
-            if(e instanceof EntityNotFoundException)
-                return ResponseEntity
-                        .status(HttpStatus.NOT_FOUND)
-                        .headers(httpHeaders)
-                        .build();
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
-        }
+
+        userService.update(userUpdateDto);
         return ResponseEntity
-                .ok()
-                .headers(httpHeaders)
+                .status(HttpStatus.OK)
                 .build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUser(@PathVariable @Positive Long id){
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Info", "Get user by username");
-        UserResponseDto userResponseDto;
-        try{
-            userResponseDto = userService.findById(id);
-        } catch (Exception e){
-            log.error("Error occurred while getting user: ", e);
-            if(e instanceof EntityNotFoundException)
-                return ResponseEntity
-                        .status(HttpStatus.NOT_FOUND)
-                        .headers(httpHeaders)
-                        .build();
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
-        }
+    public ResponseEntity<?> getUser(@PathVariable("id") @Positive Long id){
+        UserResponseDto userResponseDto = userService.findById(id);
         return ResponseEntity
-                .ok()
-                .headers(httpHeaders)
+                .status(HttpStatus.OK)
                 .body(userResponseDto);
     }
 
     @GetMapping("")
     public ResponseEntity<?> getAllUsers() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Info", "Getting all users");
-        List<UserResponseDto> users;
-        try{
-            users = userService.findAll();
-        } catch (Exception e){
-            log.error("Error occurred while getting all users: ", e);
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
-        }
+        List<UserResponseDto> users = userService.findAll();
         return users.isEmpty()
-                ? ResponseEntity.status(HttpStatus.NOT_FOUND).headers(httpHeaders).body(null)
-                : ResponseEntity.status(HttpStatus.OK).headers(httpHeaders).body(users);
+                ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+                : ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
     @DeleteMapping("/{email}")
-    public ResponseEntity<?> deleteUser(@PathVariable String email){
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("X-Info", "Deleting user");
-        try{
-            userService.deleteByEmail(email);
-        } catch (Exception e){
-            log.error("Error occurred while deleting the user: ", e);
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .headers(httpHeaders)
-                    .build();
-        }
+    public ResponseEntity<?> deleteUser(@PathVariable("email") String email){
+        userService.deleteByEmail(email);
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
-                .headers(httpHeaders)
                 .build();
     }
 
