b8fa2b3951b54c97c4f13f6e8582e78fdc61d988
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-27 02:47:08 +0100

    fix(move to confirmation_service dir)

--- aid_compass_application/src/main/java/aidcompass/api/mail/ConfirmationService.java
+++
@@ -1,9 +0,0 @@
-package aidcompass.api.mail;
-
-public interface ConfirmationService {
-
-    void sendConfirmationMessage(String recipientMail);
-
-    boolean validateConfirmationAnswer(String input);
-
-}
