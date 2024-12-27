ef966708b0d7f3cd6ce7aab5c3b744afc4682097
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-27 02:51:25 +0100

    fix(move to message_service dir)

--- aid_compass_application/src/main/java/aidcompass/api/mail/MessageService.java
+++
@@ -1,9 +0,0 @@
-package aidcompass.api.mail;
-
-import com.example.clickerapi.services.mail.models.MessageDto;
-
-public interface MessageService {
-
-    void sendMessage(MessageDto messageDto);
-
-}
