d8dbc4ddaacf265c65d4425247bd524897115db7
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-28 21:19:54 +0100

    fix(make handlers to return ResponseEntity<ExeptionDto>)

--- aid_compass_application/src/main/java/aidcompass/api/general/exeption/ControllerExceptionHandler.java
+++ aid_compass_application/src/main/java/aidcompass/api/general/exeption/ControllerExceptionHandler.java
@@ -1,59 +1,80 @@
 package aidcompass.api.general.exeption;
 
+import aidcompass.api.general.exeption.dto.ExceptionDto;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Arrays;
 
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e){
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handleThrowable(Throwable throwable, Model model) {
+        ExceptionDto exceptionDto = new ExceptionDto(
+                "500",
+                "Unexpected Internal Server Error.",
+                null);
+        log.error(throwable.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(e.getMessage());
+                .body(exceptionDto);
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<?> handleDataIntegrityViolationException(){
+    @ExceptionHandler({DataIntegrityViolationException.class, IllegalArgumentException.class})
+    public ResponseEntity<?> handleDataIntegrityViolationException(Exception e){
+        ExceptionDto exceptionDto = new ExceptionDto(
+                "400",
+                e.getMessage(),
+                Arrays.toString(e.getStackTrace()));
+        log.error("Exception 400: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .build();
+                .body(exceptionDto);
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
-
+//    @ExceptionHandler({EntityNotFoundException.class, UsernameNotFoundException.class})
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(){
+    public ResponseEntity<?> handleEntityNotFoundException(Exception e){
+        ExceptionDto exceptionDto = new ExceptionDto(
+                "404",
+                e.getMessage(),
+                null);
+        log.error("Exception 404: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .build();
+                .body(exceptionDto);
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handle(IllegalStateException e){
-        log.error(e.getMessage());
-        return "";
+    public ResponseEntity<?> handleIllegalStateException(IllegalStateException e){
+        ExceptionDto exceptionDto = new ExceptionDto(
+                "500",
+                "Illegal State Exception",
+                null);
+        log.error("Exception 500: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exceptionDto);
     }
 
 //    @ExceptionHandler(BadCredentialsException.class)
-//    public ResponseEntity<String> handle(BadCredentialsException e){
+//    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException e){
+//        ExceptionDto exceptionDto = new ExceptionDto(
+//                "400",
+//                e.getMessage(),
+//                null);
 //        return ResponseEntity
 //                .status(HttpStatus.BAD_REQUEST)
-//                .body(e.getMessage());
+//                .body(exceptionDto);
 //    }
 
 }
