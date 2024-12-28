348795412e2d6d65fe3a7face2b2c7ebc5d4ce3d
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-28 21:18:05 +0100

    add(ExceptionDto)

---
+++ aid_compass_application/src/main/java/aidcompass/api/general/exeption/dto/ExceptionDto.java
@@ -0,0 +1,6 @@
+package aidcompass.api.general.exeption.dto;
+
+public record ExceptionDto(String code,
+                           String message,
+                           String description) {
+}
