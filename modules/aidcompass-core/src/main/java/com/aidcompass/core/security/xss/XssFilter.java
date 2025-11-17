1696b3cd59f32178a770e0e96d60411f21004ca9
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-11-17 12:20:18 +0100

    fix(methods for securing)

--- modules/aidcompass-core/src/main/java/com/aidcompass/core/security/xss/XssFilter.java
+++ modules/aidcompass-core/src/main/java/com/aidcompass/core/security/xss/XssFilter.java
@@ -7,12 +7,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class XssFilter extends OncePerRequestFilter {
 
+    private final List<String> METHOD_TO_SANITIZE = List.of("POST", "PUT", "PATCH");
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!METHOD_TO_SANITIZE.contains(request.getMethod().toUpperCase())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         filterChain.doFilter(new XssHttRequestWrapper(request), response);
     }
 
