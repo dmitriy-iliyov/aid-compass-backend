32d56ab552647cf30d45097889bd56347f104b55
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-11-17 12:44:16 +0100

    fix(from list to set)

--- modules/aidcompass-core/src/main/java/com/aidcompass/core/security/xss/XssFilter.java
+++ modules/aidcompass-core/src/main/java/com/aidcompass/core/security/xss/XssFilter.java
@@ -8,11 +8,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 
 public class XssFilter extends OncePerRequestFilter {
 
-    private final List<String> METHOD_TO_SANITIZE = List.of("POST", "PUT", "PATCH");
+    private final Set<String> METHOD_TO_SANITIZE = Set.of("POST", "PUT", "PATCH");
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
